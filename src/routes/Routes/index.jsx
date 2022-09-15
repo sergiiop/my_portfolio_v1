226ecd70f913121f80de747f753ec5a8f73b9a43
Sergio Pérez <sergio-luis98@hotmail.com>
2022-09-15 01:02:40 -0500

    fix/hashrouter

--- src/routes/Routes/index.jsx
+++ src/routes/Routes/index.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom'
+import { HashRouter, Route, Routes as Switch } from 'react-router-dom'
 import { useViews } from '../../views'
 
 const Routes = () => {
   const { useScreen } = useViews()
   const { Home, Resume, Projects } = useScreen()
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path='/' element={<Home />}></Route>
         <Route path='/resume' element={<Resume />}></Route>
         <Route path='/projects' element={<Projects />}></Route>
         <Route path='*' element={<h1>Not Found</h1>} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
